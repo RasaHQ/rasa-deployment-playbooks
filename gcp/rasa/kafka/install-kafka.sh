4d45244228c2eebd1f037349fc7af916acb90f03
Jamie MacDonald <j.macdonald@rasa.com>
2025-07-24 16:26:11 +0100

    Add Kafka docs, remove generated values files

---
+++ gcp/rasa/kafka/install-kafka.sh
@@ -0,0 +1,37 @@
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Generating kubeconfig to authenticate with GKE cluster..."
+# To be able to interact with the GKE cluster we deployed earlier, we need to obtain the credentials for it. These credentials are saved in a file called kubeconfig which the gcloud CLI can generate for us and kubectl can use.
+# Ensure we've got a path setup for the kubeconfig file:
+export KUBECONFIG=$(pwd)/kubeconfig
+echo "Kubeconfig path:  $KUBECONFIG"
+rm -f $KUBECONFIG
+#Retrieve the credentials for the cluster using the gcloud CLI:
+gcloud container clusters get-credentials $NAME --region=$REGION 
+# Next, validate that the credentials work - we should see information about our cluster output here if everything has worked.
+echo "Kubeconfig generated successfully! Printing cluster info below, if you see output here, authentication was successful."
+kubectl cluster-info
+kubectl get ns
+
+# Create a namespace for all the Rasa products to live in
+kubectl create namespace $NAMESPACE
+kubectl label namespace $NAMESPACE istio-injection=enabled
+
+# Create a random 16 character password for Kafka to use for authentication and then inject it into the Kafka configuration file.
+echo "Generating a random 16 character password for Kafka to use for authentication..."
+export KAFKA_PASSWORD=$(openssl rand -hex 16)
+envsubst < $SCRIPT_DIR/kafka.template.yaml > $SCRIPT_DIR/kafka.yaml
+echo "Kafka password: $KAFKA_PASSWORD"
+echo "Kafka configuration file generated successfully!"
+
+# We'll fetch the automatically generated password from the previous step, and then use it to create a couple of configuration files locally that act as configuration for Rasa to be able to connect to Kafka
+echo "Installing Kafka to the cluster..."
+helm uninstall kafka -n $NAMESPACE
+helm upgrade --install -n $NAMESPACE kafka bitnami/kafka -f values/kafka.yaml --version 32.3.2
+echo "Kafka installed successfully!"
+
+echo "Generating client configuration for Rasa to use to connect to Kafka..."
+envsubst < $SCRIPT_DIR/client.properties.template > $SCRIPT_DIR/client.properties
+envsubst < $SCRIPT_DIR/kafka_jaas.conf.template > $SCRIPT_DIR/kafka_jaas.conf
+echo "Client configuration for Rasa to use to connect to Kafka generated successfully!"
