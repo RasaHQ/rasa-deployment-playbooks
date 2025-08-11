207f2846b33c6896e6d969371b7c6a5cfbfc230d
Jamie MacDonald <j.macdonald@rasa.com>
2025-08-11 09:44:38 +0100

    Remove AWS scripts for initial publication

--- aws/configure/get-infra-values.sh
+++
@@ -1,8 +0,0 @@
-echo "Fetching some infrastructure values..."
-export DB_HOST=$(gcloud sql instances describe $NAME --format='value(ipAddresses[0].ipAddress)')
-export REDIS_HOST=$(gcloud redis instances describe $NAME --region=$REGION --format='value(host)')
-export REDIS_AUTH=$(gcloud redis instances get-auth-string $NAME --region=$REGION --format='value(authString)')
-echo "Infrastructure values fetched successfully:"
-echo "DB_HOST: $DB_HOST"
-echo "REDIS_HOST: $REDIS_HOST"
-echo "REDIS_AUTH: $REDIS_AUTH"
