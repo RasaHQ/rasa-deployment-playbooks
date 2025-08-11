c9781ceee32db157521876310bcb0c19e32f8c26
Jamie MacDonald <j.macdonald@rasa.com>
2025-08-11 17:31:01 +0100

    Update configuration scripts for AWS

--- aws/configure/configure-cluster.sh
+++ aws/configure/configure-cluster.sh
@@ -2,14 +2,14 @@ set -e
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "Generating kubeconfig to authenticate with GKE cluster..."
-# To be able to interact with the GKE cluster we deployed earlier, we need to obtain the credentials for it. These credentials are saved in a file called kubeconfig which the gcloud CLI can generate for us and kubectl can use.
+echo "Generating kubeconfig to authenticate with AWS EKS cluster..."
+# To be able to interact with the EKS cluster we deployed earlier, we need to obtain the credentials for it. These credentials are saved in a file called kubeconfig which the AWS CLI can generate for us and kubectl can use.
 # Ensure we've got a path setup for the kubeconfig file:
 export KUBECONFIG=$(pwd)/kubeconfig
 echo "Kubeconfig path:  $KUBECONFIG"
 rm -f $KUBECONFIG
-#Retrieve the credentials for the cluster using the gcloud CLI:
-gcloud container clusters get-credentials $NAME --region=$REGION 
+#Retrieve the credentials for the cluster using the AWS CLI:
+aws eks update-kubeconfig --region $REGION --name $NAME
 # Next, validate that the credentials work - we should see information about our cluster output here if everything has worked.
 echo "Kubeconfig generated successfully! Printing cluster info below, if you see output here, authentication was successful."
 kubectl cluster-info
@@ -26,8 +26,6 @@ $ISTIO version
 
 # Install Istio onto Your Cluster
 # Use our preconfigured YAML files to install Istio onto your cluster.
-
-
 echo "Installing Istio on your cluster..."
 $ISTIO install --set profile=demo --skip-confirmation -f "$SCRIPT_DIR/istio-operator.yaml"
 
@@ -35,14 +33,8 @@ $ISTIO install --set profile=demo --skip-confirmation -f "$SCRIPT_DIR/istio-oper
 echo "Creating the Istio Ingress Class on your cluster..."
 kubectl apply -f "$SCRIPT_DIR/istio-ingress-class.yaml"
 
-# Create DNS Zone
-# We will now create a DNS zone on Google Cloud. We'll use this to map our own domain name like rasa.example.com onto the Rasa products that we deploy and ensure they can be accessed by domain name.
-gcloud dns managed-zones create $DNS_ZONE \
-  --dns-name=$DOMAIN \
-  --description="$NAME"
-
 # You will now need to update some DNS records on your domain. You will need to find where your DNS is configured for your domain - this may be a cloud provider like AWS or a domain registrar like GoDaddy or Cloudflare.
 # Retrieve the the nameservers of the zone you have just created in GCP:
 echo "Retrieving the nameservers of the zone you have just created in GCP..."
 echo "You must now create an NS record for your domain $DOMAIN with the following values:"
-gcloud dns record-sets list --zone=$DNS_ZONE --name=$DOMAIN --type=NS
+echo "TODO: Add NS records command here"
