bafd2fbc2b23bdbdc127a78171be54dcb021c40d
Bojan Petrovic <b.petrovic@rasa.com>
2025-08-18 18:29:39 +0200

    Add kubeconfig to AWS configure-cluster.sh

--- aws/configure/configure-cluster.sh
+++ aws/configure/configure-cluster.sh
@@ -1,4 +1,21 @@
 set -e
+
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo "Generating kubeconfig to authenticate with EKS cluster..."
+# To be able to interact with the EKS cluster we deployed earlier, we need to obtain the credentials for it. These credentials are saved in a file called kubeconfig which the AWS CLI can generate for us and kubectl can use.
+# Ensure we've got a path setup for the kubeconfig file:
+export KUBECONFIG=$(pwd)/kubeconfig
+echo "Kubeconfig path:  $KUBECONFIG"
+rm -f $KUBECONFIG
+#Retrieve the credentials for the cluster using the AWS CLI:
+aws eks update-kubeconfig --region $REGION --name $NAME
+# Next, validate that the credentials work - we should see information about our cluster output here if everything has worked.
+echo "Kubeconfig generated successfully! Printing cluster info below, if you see output here, authentication was successful."
+kubectl cluster-info
+kubectl get ns
+
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
