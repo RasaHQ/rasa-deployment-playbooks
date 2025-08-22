43012df57b346aae68fc7497ae22aecb3a42e6b5
Bojan Petrovic <b.petrovic@rasa.com>
2025-08-22 16:42:03 +0200

    Change color and add prefix for info and error logs

--- utils/common.sh
+++ utils/common.sh
@@ -2,10 +2,10 @@ trap 'if [ $? -ne 0 ]; then print_error "Script failed!" >&2; fi' EXIT
 
 # Function to print text in green color
 print_info() {
-    printf "\033[32m%s\033[0m\n" "$1"
+    printf "\033[36m--->\033[0m \033[1m%b\033[22m\n" "$1"
 }
 
 # Function to print text in red color
 print_error() {
-    printf "\033[31m%s\033[0m\n" "$1"
+    printf "\033[1;31m---> %b\033[0m\n" "$1"
 }
