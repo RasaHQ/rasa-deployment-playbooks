25b07ee5ba62f09f6805b731b19c2aa884343d50
Bojan Petrovic <b.petrovic@rasa.com>
2025-12-05 15:20:06 +0100

    Azure: Remove admin access from db users

--- azure/setup/environment-variables.sh
+++ azure/setup/environment-variables.sh
@@ -46,10 +46,6 @@ export DB_ASSISTANT_USERNAME="assistant"
 export DB_STUDIO_DATABASE="studio"
 # The username for the Rasa Studio database.
 export DB_STUDIO_USERNAME="studio"
-# The database name for Keycloak.
-export DB_KEYCLOAK_DATABASE="keycloak"
-# The username for the Keycloak database.
-export DB_KEYCLOAK_USERNAME="keycloak"
 # The version of PostgreSQL Container to use for applying some configuration to the database.
 export PG_VERSION=17
 #--------------------------------
@@ -86,8 +82,6 @@ echo "DB assistant database:       $DB_ASSISTANT_DATABASE"
 echo "DB assistant username:       $DB_ASSISTANT_USERNAME"
 echo "DB studio database:          $DB_STUDIO_DATABASE"
 echo "DB studio username:          $DB_STUDIO_USERNAME"
-echo "DB keycloak database:        $DB_KEYCLOAK_DATABASE"
-echo "DB keycloak username:        $DB_KEYCLOAK_USERNAME"
 echo "PostgreSQL version:          $PG_VERSION"
 echo "--------------------------------"
 echo "If any of the above values are incorrect or blank, please update the file and re-run."
